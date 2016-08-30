d4c7df4407fb67b67b11e471907067fd807b80bb
BillTube <billbt9010@gmail.com>
2016-08-30 20:30:04 +0200

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -684,7 +684,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-53755606-1', 'auto');
 ga('send', 'pageview');
 
-var bgColorArray = ['http://i.imgur.com/zFlCqZP.jpg','http://i.imgur.com/l622wXI.jpg','http://i.imgur.com/mTMoAeX.jpg','http://i.imgur.com/lpFfKYs.jpg','http://i.imgur.com/02154Tn.jpg','http://i.imgur.com/y2tW3bM.jpg','http://i.imgur.com/GNHovtU.jpg','http://i.imgur.com/m101zRQ.jpg','http://i.imgur.com/c4U0NJ9.jpg','http://i.imgur.com/BN1Yisy.jpg'],
+var bgColorArray = ['//i.imgur.com/zFlCqZP.jpg','//i.imgur.com/l622wXI.jpg','//i.imgur.com/mTMoAeX.jpg','//i.imgur.com/lpFfKYs.jpg','//i.imgur.com/02154Tn.jpg','//i.imgur.com/y2tW3bM.jpg','//i.imgur.com/GNHovtU.jpg','//i.imgur.com/m101zRQ.jpg','//i.imgur.com/c4U0NJ9.jpg','//i.imgur.com/BN1Yisy.jpg','//i.imgur.com/qmjkll4.jpg','//i.imgur.com/2jSsg8N.jpg'],
     selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 
 $('#backg').css('background', 'url(' + selectBG + ')')
