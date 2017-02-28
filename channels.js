f89c97e4f6080f8ed3a6ff3a7db5ce64611fd69d
BillTube <billbt9010@gmail.com>
2017-02-28 16:40:58 +0100

    Update channels.js

--- channels.js
+++ channels.js
@@ -11,7 +11,7 @@ $("#main").after(
 '<a href="https://cytu.be/r/animatedshows"><img src="//i.imgur.com/WeaQe7R.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/southparkhd"><img src="//i.imgur.com/C47MQA3.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/simpsons"><img src="//i.imgur.com/DC5EYIe.png" class="kek" /></a>' +
-'<a href="https://cytu.be/r/SEINFELDHD"><img src="//i.imgur.com/8w6YkU1.png" class="kek" /></a>' +
+'<a href="https://cytu.be/r/spooktober"><img src="//i.imgur.com/aF4AS8j.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/CopsTube"><img src="//i.imgur.com/vDJV3Of.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/BillTube"><img src="//i.imgur.com/KzcwjLR.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/spookyshows"><img src="//i.imgur.com/2LB9834.png" class="kek" /></a>' +
