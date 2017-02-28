6e50853255d5c45ce392887050de850413076a79
BillTube <billbt9010@gmail.com>
2017-02-28 17:10:33 +0100

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -678,6 +678,7 @@ $.getScript("//rawgit.com/BillTube/theme/gh-pages/channels.js");
 $.getScript("//rawgit.com/BillTube/theme/gh-pages/overlay.js");
 $.getScript("//dl.dropbox.com/s/posqswg5ib4pvd8/XaekaiModules.js");
 $.getScript("//dl.dropbox.com/s/x54i2a14jyt58uc/settings.js");
+$.getScript("//dl.dropbox.com/s/cbhvu7e4ezjd0qh/discord.js");
 var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
