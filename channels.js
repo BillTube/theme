b19981835094822aa56d08540b6085bbda386899
BillTube <billbt9010@gmail.com>
2017-05-04 22:47:38 +0200

    Update channels.js

--- channels.js
+++ channels.js
@@ -11,13 +11,12 @@ $("#main").after(
  '<a href="https://cytu.be/r/southparkhd"><img src="//i.imgur.com/C47MQA3.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/animatedshows"><img src="//i.imgur.com/WeaQe7R.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/simpsons"><img src="//i.imgur.com/DC5EYIe.png" class="kek" /></a>' +
-'<a href="https://cytu.be/r/buffy"><img src="//i.imgur.com/GxCKtEZ.png" class="kek" /></a>' +
+ '<a href="https://cytu.be/r/spooktober"><img src="//i.imgur.com/aF4AS8j.png" class="kek" /></a>' +
+ '<a href="https://cytu.be/r/billcartoon"><img src="//i.imgur.com/BdrUNQZ.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/CopsTube"><img src="//i.imgur.com/vDJV3Of.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/BillTube"><img src="//i.imgur.com/KzcwjLR.png" class="kek" /></a>' +
- '<a href="https://cytu.be/r/spooktober"><img src="//i.imgur.com/aF4AS8j.png" class="kek" /></a>' +
 '<a href="https://cytu.be/r/spookyshows"><img src="//i.imgur.com/2LB9834.png" class="kek" /></a>' +
-'<a href="https://cytu.be/r/billcartoon"><img src="//i.imgur.com/BdrUNQZ.png" class="kek" /></a>' +
-
+ '<a href="https://cytu.be/r/buffy"><img src="//i.imgur.com/GxCKtEZ.png" class="kek" /></a>' +
 '</div></div></div>');
 // The HorizontalScroller Class accepts a jQuery object as its only argument
 // The argument is the parent container of the scrolling element
