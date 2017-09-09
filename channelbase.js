6f4fb04608ee1237ba58eddf19b4f183e94ed068
BillTube <billbt9010@gmail.com>
2017-09-09 04:11:26 +0200

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -84,10 +84,6 @@ $("#ploptions").append($("#shuffleplaylist"), $("#clearplaylist"), $("#getplayli
 $("#pldropdown").before($("#qlockbtn"));
 $("#main").after($("#scroll-feature"));
 $("<div class='indicator'><svg width='16px' height='12px'><polyline id='back' points='1 6 4 6 6 11 10 1 12 6 15 6'></polyline><polyline id='front' points='1 6 4 6 6 11 10 1 12 6 15 6'></polyline></svg></div>").appendTo('.navbar-header');
-$('.username').hover(
-     function(){ $('.timestamp').addClass('fadeout') },
-     function(){ $('.timestamp').removeClass('fadeout') }
-)
 $('#queuecontainer').hover(function(){
     $('#pllength').css({
         'opacity':'0.9',
