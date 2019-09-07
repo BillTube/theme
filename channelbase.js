a1c02b63f60a71663bdee2f874b7e9a1dd2c7620
BillTube <billbt9010@gmail.com>
2019-09-07 13:19:35 +0200

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -709,3 +709,11 @@ $("body").addClass('fluid');
 //testing this out
 $.getScript("//dl.dropbox.com/s/39iz5fdgzxufp7b/lazyloadbilltheme.js");
 //could go terribly wrong!
+var field = document.querySelectorAll('[data-persist]')
+for (i=0;i<field.length;i++){
+  var stored = localStorage.getItem(field[i].getAttribute('data-persist'))
+  if (stored) field[i].value = stored
+  field[i].addEventListener('input',function(){
+    localStorage.setItem(this.getAttribute('data-persist'),this.value)
+  })
+}
