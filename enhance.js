91ce1e910cf2b9c24406120e3172fd380bed0e28
BillTube <billbt9010@gmail.com>
2019-09-13 19:10:35 +0200

    Update enhance.js

--- enhance.js
+++ enhance.js
@@ -1,3 +1,6 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+}else {
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * jQuery Mousewheel 3.1.13
@@ -3174,3 +3177,4 @@ videojs("ytapiplayer_html5_api").ready(function(){
 });
 
 },{}]},{},[4,3,5,7,16,19]);
+}
